9aea1851cd992b0f68cd54f5a22538493f24a099
Francois Martin <contact@francois-martin.com>
2022-10-16 12:49:44 +0200

    Layout update / cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <el-config-provider namespace="ep">
-    <BaseHeader />
-    <div style="display: flex">
+    <div style="display: flex; height: 100vh">
       <BaseSide />
-      <div>
-        <img alt="Vue logo" class="element-plus-logo" src="./assets/logo.png" />
-        <HelloWorld msg="Hello Vue 3.0 + Element Plus + Vite" />
-      </div>
     </div>
   </el-config-provider>
 </template>
@@ -15,9 +10,5 @@
 #app {
   text-align: center;
   color: var(--ep-text-color-primary);
-}
-
-.element-plus-logo {
-  width: 50%;
 }
 </style>
