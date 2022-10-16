18a0aecc7ebfaa399d555b52954906c579e0dfee
Francois Martin <contact@francois-martin.com>
2022-10-16 16:31:03 +0200

    Add main views

--- src/components/layouts/BaseSide.vue
+++ src/components/layouts/BaseSide.vue
@@ -1,20 +1,26 @@
 <template>
-  <el-tabs v-model="activeName" tab-position="left">
-    <el-tab-pane label="User" name="first">
+  <el-tabs tab-position="left" v-model="activeTab">
+    <el-tab-pane label="General" name="general">
+      <GeneralView/>
+    </el-tab-pane>
+    <el-tab-pane label="Users" name="users">
+      <UsersView/>
+    </el-tab-pane>
+    <el-tab-pane label="Commands" name="commands">
+      <CommandsView/>
+    </el-tab-pane>
+    <el-tab-pane label="Task" name="fourth">
       <div>
         <HelloWorld msg="Hello Vue 3.0 + Element Plus + Vite" />
       </div>
     </el-tab-pane>
-    <el-tab-pane label="Config" name="second">Config</el-tab-pane>
-    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
-    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
   </el-tabs>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const activeName = ref('first')
+const activeTab = ref('first')
 </script>
 
 <style>
---
+++ src/components/views/UsersView.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-table :data="tableData">
+    <el-table-column prop="username" label="Username" align="center"/>
+    <el-table-column prop="authtype" label="Authentication" align="center"/>
+    <el-table-column label="" align="center">
+      <template #default="scope">
+        <el-button link type="primary" @click="edit(scope.$index, scope.row)">Edit</el-button>
+        <el-button link type="primary">Remove</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script lang="ts" setup>
+interface User {
+  username: string;
+  authtype: string;
+}
+
+const edit = (index: number, row: User) => {
+  console.log(index);
+  console.log(row);
+}
+
+const tableData = [
+  {
+    username: 'foo',
+    authtype: 'key'
+  },
+  {
+    username: 'bar',
+    authtype: 'key'
+  },
+]
+</script>
+
+<style>
+</style>
